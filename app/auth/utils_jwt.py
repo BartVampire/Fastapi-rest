3b3abfb53fcec8120bb3d928c61a4fbc9e16f234
anonymous <nogolev6@gmail.com>
2024-10-14 19:26:33 +0300

    Добавлены функции для кодирования и декодирования токенов

--- app/auth/utils_jwt.py
+++ app/auth/utils_jwt.py
@@ -1,5 +1,20 @@
-from app
+import jwt
+from app.core.config import settings
 
-def encode_jwt():
-    pass
 
+def encode_jwt(
+        payload: dict,
+        private_key: str =settings.auth.private_key_path.read_text(),
+        algorithm: str =settings.auth.algorithm
+): # Функция кодирования токена JWT с использованием RS256 алгоритма
+    encoded = jwt.encode(payload, private_key, algorithm=algorithm)
+    return encoded
+
+
+def decode_jwt(
+        token: str | bytes,
+        public_key: str = settings.auth.public_key_path.read_text(),
+        algorithms: str = settings.auth.algorithm
+): # Функция декодирования токена JWT с использованием RS256 алгоритма
+    decoded = jwt.decode(token, public_key, algorithms=[algorithms])
+    return decoded
